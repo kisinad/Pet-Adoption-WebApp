1585ed8de06ca39c0472163e212652ebd4120afb
Denis Kisina <kisinad@gmail.com>
2020-05-12 10:57:56 -0500

    added url to hit

--- src/main/java/edu/miu/cs/cs427/blackpanther/controller/PetRegistrationServlet.java
+++ src/main/java/edu/miu/cs/cs427/blackpanther/controller/PetRegistrationServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "PetRegistrationServlet")
+@WebServlet(name = "PetRegistrationServlet", urlPatterns = {"/register-pet"})
 public class PetRegistrationServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
